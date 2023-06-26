d601e9b239454d04e2c80193e55fe23321449b9e
Marcos <marcosugaldemartinez@gmail.com>
2023-06-26 21:00:10 +0200

    feat: created sql tables

--- back/sql/creation.sql
+++ back/sql/creation.sql
@@ -1,3 +1,4 @@
+DROP TABLE IF EXISTS exercises;
 DROP TABLE IF EXISTS sets;
 DROP TABLE IF EXISTS workouts;
 DROP TABLE IF EXISTS users;
@@ -22,7 +23,19 @@ CREATE TABLE IF NOT EXISTS workouts (
 CREATE TABLE IF NOT EXISTS sets (
     id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
     set_name TEXT NOT NULL,
+    reps INTEGER,
+    rest_time INTEGER,
     workout_id uuid NOT NULL REFERENCES workouts
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS exercises (
+    id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+    name TEXT NOT NULL,
+    description TEXT,
+    duration TEXT NOT NULL,
+    set_id uuid NOT NULL REFERENCES sets
         ON UPDATE CASCADE
         ON DELETE CASCADE
 );
