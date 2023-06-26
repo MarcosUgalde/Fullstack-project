3b47f8450beb165a67b11fbfdcdc154c131c25fc
Marcos <marcosugaldemartinez@gmail.com>
2023-06-26 21:19:06 +0200

    feat: started backend configuration

---
+++ back/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const { db } = require("./configs");
+const errors = require("./misc/errors");
+
+const app = express();
+
+app.use(cors(options));
+app.use(express.json());
+app.use(cookieParser());
+
+const routes = require("./routes");
+
+app.use(routes(db));
+
+app.use((_, _, next) => {
+  next(errors[404]);
+});
+
+app.listen(process.env.PORT, () =>
+  console.info(`> listening at: ${process.env.PORT}`)
+);
