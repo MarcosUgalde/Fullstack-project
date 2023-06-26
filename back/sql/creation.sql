5c97433622ac2977d9dfb36aa967dbcbe5e13259
Marcos <marcosugaldemartinez@gmail.com>
2023-06-26 19:23:52 +0200

    feat: created users table

---
+++ back/sql/creation.sql
@@ -0,0 +1,8 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE IF NOT EXISTS users (
+    id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+    email TEXT NOT NULL UNIQUE,
+    username TEXT NOT NULL, 
+    password TEXT NOT NULL
+);
