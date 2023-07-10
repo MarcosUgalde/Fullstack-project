28a579875398706b28301226830b1b7534e17c5f
Marcos <marcosugaldemartinez@gmail.com>
2023-07-10 13:45:44 +0200

    feat: started page for creating a new workout

--- front/src/pages/Createworkout/index.jsx
+++ front/src/pages/Createworkout/index.jsx
@@ -4,7 +4,22 @@ function Createworkout() {
     return (
         <Styled.Body>
             <h1>Create a new workout!</h1>
-            
+            <Styled.Form>
+                <input type='text' id='workout' placeholder='Insert Workout Name'></input>
+                <input type="text" placeholder='Insert set name'></input>
+                <input type='text' placeholder='Exercise name'></input>
+                <input type='text' placeholder='Description (optional)'></input>
+                <input type='text' placeholder='Duration (seconds)'></input>
+                <ul>
+                    <li></li>
+                </ul>
+                <label htmlFor='reps'>Insert repetitions</label>
+                <input id='reps'></input>
+                <label htmlFor='rest'>Insert rest time (seconds)</label>
+                <input id='rest' placeholder='ex: 30'></input>
+            </Styled.Form>
+            <button>Add set</button>
+            <input type='submit'></input>
         </Styled.Body>
     )
 }
--- front/src/pages/Createworkout/styles.jsx
+++ front/src/pages/Createworkout/styles.jsx
@@ -2,9 +2,18 @@ import { styled } from 'styled-components'
 
 const Body = styled.section`
     display: flex;
-    flex-directio: column;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`
+const Form = styled.section`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
 `
 
 export default {
     Body,
+    Form,
 }
