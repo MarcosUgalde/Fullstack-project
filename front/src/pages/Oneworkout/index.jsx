c4f56b3e9cb797334797192c0f2f260b08c5278f
Marcos <marcosugaldemartinez@gmail.com>
2023-07-17 19:53:06 +0200

    feat: added main features of the workout page without functionality

--- front/src/pages/Oneworkout/index.jsx
+++ front/src/pages/Oneworkout/index.jsx
@@ -1,8 +1,18 @@
+import Styled from './styles'
+
 function Oneworkout() {
     return (
-        <>
+        <Styled.Body>
             <h1>Workout name</h1>
-        </>
+            <h3>Set name</h3>
+            <Styled.Block>
+                <p>Exercise name</p>
+                <p>Time remaining of exercise</p>
+                <p>Description</p>
+            </Styled.Block>
+            <button>Pause</button>
+            <button>Stop workout</button>
+        </Styled.Body>
     )
 }
 
---
+++ front/src/pages/Oneworkout/styles.jsx
@@ -0,0 +1,20 @@
+import { styled } from 'styled-components' 
+
+const Body = styled.section`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`
+
+const Block = styled.section`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`
+
+export default {
+    Body,
+    Block,
+}
