1822642e974d10f47816f0186f0c4fdd5c009b6e
Marcos <marcosugaldemartinez@gmail.com>
2023-07-17 19:58:50 +0200

    fix: modified workout page

--- front/src/pages/Oneworkout/index.jsx
+++ front/src/pages/Oneworkout/index.jsx
@@ -3,15 +3,32 @@ import Styled from './styles'
 function Oneworkout() {
     return (
         <Styled.Body>
-            <h1>Workout name</h1>
-            <h3>Set name</h3>
-            <Styled.Block>
-                <p>Exercise name</p>
-                <p>Time remaining of exercise</p>
-                <p>Description</p>
-            </Styled.Block>
-            <button>Pause</button>
-            <button>Stop workout</button>
+            <>
+                <h1>Workout name</h1>
+                <h3>Set 1</h3>
+                <ul>
+                    <li>Exercise 1</li>
+                    <li>Exercise 2</li>
+                    <li>Exercise 3</li>
+                </ul>
+                <h3>Set 2</h3>
+                <ul>
+                    <li>Exercise 1</li>
+                    <li>Exercise 2</li>
+                    <li>Exercise 3</li>
+                </ul>
+            </>
+            <>
+                <h1>Workout name</h1>
+                <h3>Set name</h3>
+                <Styled.Block>
+                    <p>Exercise name</p>
+                    <p>Time remaining of exercise</p>
+                    <p>Description</p>
+                </Styled.Block>
+                <button>Pause</button>
+                <button>Stop workout</button>
+            </>
         </Styled.Body>
     )
 }
