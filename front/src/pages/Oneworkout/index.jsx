4c9aa8940467f80b44e4257f7de6a819132ced4e
Marcos <marcosugaldemartinez@gmail.com>
2023-07-19 16:33:24 +0200

    feat: one workout page gets all info needed from DB

--- front/src/pages/Oneworkout/index.jsx
+++ front/src/pages/Oneworkout/index.jsx
@@ -2,7 +2,7 @@ import Styled from './styles'
 import { workout } from '../../services'
 import { useQuery } from "react-query";
 import { useUrl } from '../../hooks'
-//import { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function Oneworkout() {
     const url = useUrl()
@@ -11,6 +11,47 @@ function Oneworkout() {
     if(isLoading) return <div><p>Loading</p></div>
     const exercises = data.data.map((item) => item)
     console.log(exercises)
+    const index = 0;
+    
+    //const [currentIndex, setCurrentIndex] = useState(0);
+    //const [remainingDuration, setRemainingDuration] = useState(0);
+    
+    // const [exercises, setExercises] = useState([])
+    //const [started, setStarted] = useState(false)
+    
+    /*useEffect(() => {
+        if(!isLoading && !started) {
+            if(data && data.data && Array.isArray(data.data) && data.data.length > 0) {
+                setExercises(data.data);
+                setCurrentIndex(0);
+                setRemainingDuration(data.data[0].duration)
+            }
+        }
+    }, [data, isLoading, started])
+    */
+/** 
+    const startWorkout = () => {
+       setCurrentIndex(0);
+       setRemainingDuration(exercises[0].duration)
+   }
+   */
+   /* 
+   useEffect(() => {
+       if(remainingDuration === 0) {
+           if(currentIndex + 1 < exercises.length) {
+               setCurrentIndex((prevIndex) => prevIndex +1);
+               setRemainingDuration(exercises[currentIndex + 1].duration)
+           }
+       } else {
+           const interval = setInterval (() => {
+               setRemainingDuration((prevDuration) => prevDuration -1);
+           }, 1000)
+           return () => clearInterval(interval)
+       }
+   }, [currentIndex, remainingDuration, exercises])
+    
+    */
+
 
     return (
         <Styled.Body>
@@ -29,9 +70,9 @@ function Oneworkout() {
             <>
                 <h3>{data.data[0].set_name}</h3>
                 <Styled.Block>
-                    <p>{exercises[0].exercise_name}</p>
-                    <p>{exercises[0].duration}</p>
-                    <p>{exercises[0].description}</p>
+                    <p>{exercises[index].exercise_name}</p>
+                    <p>{exercises[index].duration}</p>
+                    <p>{exercises[index].description}</p>
                 </Styled.Block>
                 <button>Pause</button>
                 <button>Stop Workout</button>
