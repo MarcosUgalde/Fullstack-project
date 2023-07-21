86dee7ab29891b72e8848c03980ccd26620f6a94
Marcos <marcosugaldemartinez@gmail.com>
2023-07-21 16:01:30 +0200

    feat: added navbar

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -3,16 +3,31 @@ import ReactDOM from 'react-dom/client'
 import Provider from './context/Provider'
 import Register from './pages/Register'
 import Login from './pages/Login'
-import { Switch, Route, Redirect } from 'wouter'
+import { Switch, Route, Redirect, Link, useRoute } from 'wouter'
 import Mainmenu from './pages/Mainmenu'
 import Createworkout from './pages/Createworkout'
 import Guard from './components/Guard'
 import Allworkouts from './pages/Allworkouts'
 import Oneworkout from './pages/Oneworkout'
 
+const CustomLink = ({ href, children }) => {
+  const [isActive] = useRoute(href)
+
+  return (
+    <Link {...{ href }}>
+      <a {...{ href }}>{children}</a>
+    </Link>
+  )
+}
+
 const Main = () => {
   return (
     <Provider>
+      <nav>
+        <CustomLink href='/'>Menu</CustomLink>
+        <CustomLink href='/workouts'>Workouts</CustomLink>
+      </nav>
+      
       <Switch>
         <Route path='/signup' component={Register} />
         <Route path='/login' component={Login}/>
