8945b14156e499cc47bd555644b843dd48e808a4
Marcos <marcosugaldemartinez@gmail.com>
2023-07-22 13:41:46 +0200

    fix: deleted usless button

--- front/src/pages/Createworkout/index.jsx
+++ front/src/pages/Createworkout/index.jsx
@@ -18,12 +18,6 @@ function Createworkout() {
         console.log(data)
         handleNewSet(data.data.id)
     }})
-    //console.log(data)
-    //const { mutate: setMutate,data: setData } = useMutation((payload) => workout.addset({ payload }))
-
-    //const { mutate: setMutateExercise,data: setExerciseData } = useMutation((payload) => workout.addexercise({ payload }))
-   // console.log(setExerciseData)
-
     
     return (
         <Styled.Body>
@@ -40,7 +34,6 @@ function Createworkout() {
                 return element
             })}
 
-            <button onClick={handleNewSet}>New Set</button>
             <Link to='/workouts'>
                 <button>Save</button>
             </Link>
@@ -50,49 +43,5 @@ function Createworkout() {
 
 export default Createworkout
 
-            /*    <Styled.Form>
-                <input type="text" name='setName' placeholder='Insert set name' onChange={(e) => {
-                    setPayload({
-                        ...payload,
-                        [e.target.name]: e.target.value,
-                    })
-                }}></input>
-                <label htmlFor='reps'>Insert repetitions</label>
-                <input id='reps' name='rounds' onChange={(e) => {
-                    setPayload({
-                        ...payload, 
-                        [e.target.name]: e.target.value
-                    })
-                }}></input>
-                <label htmlFor='rest'>Insert rest time (seconds)</label>
-                <input id='rest' name='rest_time' placeholder='ex: 30' onChange={(e) => {
-                    setPayload({
-                        ...payload,
-                        [e.target.name]: e.target.value
-                    })
-                }}></input>
-                <button onClick={() => { setMutate({...payload, workoutId: data.data.id}) }}>Continue</button>
-                <input type='text' name='exerciseName' placeholder='Exercise name' onChange={(e) => {
-                    setPayload({
-                        ...payload,
-                        [e.target.name]: e.target.value,
-                    })
-                }}></input>
-                <input type='text' name='description' placeholder='Description (optional)' onChange={(e) => {
-                    setPayload({
-                        ...payload,
-                        [e.target.name]: e.target.value,
-                    })
-                }} ></input>
-                <input type='text' name='duration' placeholder='Duration (seconds)' onChange={(e) => {
-                    setPayload({
-                        ...payload,
-                        [e.target.name]: e.target.value,
-                    })
-                }} ></input>
-                <button onClick={() => setMutateExercise({ ...payload, setId: setData.data.id })}>Add Exercise</button>
-                <ul>
-                    <li></li>
-                </ul>
-            </Styled.Form>
-             */
+           
+           // <button onClick={handleNewSet}>New Set</button>
