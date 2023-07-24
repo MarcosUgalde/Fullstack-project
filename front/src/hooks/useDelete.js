17587fa24a638b550ce494a52e52eb41e9bbe1cb
Marcos <marcosugaldemartinez@gmail.com>
2023-07-24 13:03:25 +0200

    fix: fixed bug in useDelete hook

--- front/src/hooks/useDelete.js
+++ front/src/hooks/useDelete.js
@@ -1,12 +1,14 @@
 import { useQueryClient, useMutation } from "react-query";
+import { useLocation } from "wouter";
 import { workout } from "../services";
 
 export const useDelete = () => {
   const queryClient = useQueryClient();
+  const [, setLocation] = useLocation();
 
   const deleteMutation = useMutation((name) => workout.deleteWorkout(name), {
     onSuccess: () => {
-      queryClient.invalidateQueries("workouts");
+      queryClient.invalidateQueries("workouts") && setLocation("/");
     },
   });
 
